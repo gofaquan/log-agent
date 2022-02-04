1191d806c03e2032f7a64e6c79c80cbf43609521
gofaquan <2660796265@qq.com>
2022-02-04 19:01:46 +0800

    init config.ini and main.go struct and load code

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/gofaquan
+
+go 1.17
+
+require (
+	github.com/go-ini/ini v1.66.3
+	github.com/sirupsen/logrus v1.8.1
+)
+
+require (
+	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"github.com/go-ini/ini"
+	"github.com/sirupsen/logrus"
+)
+
+type config struct {
+}
+
 func main() {
+	// 1. 读配置文件 `go-ini`
+	var configObj = new(config)
+	err := ini.MapTo(configObj, "./conf/config.ini")
+	if err != nil {
+		logrus.Error("load config failed:%v", err)
+	}
+	fmt.Println(".ini file load success !")
 
 }
